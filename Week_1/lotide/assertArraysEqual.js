b11f658331dc3246b97e665ce6178b18e005376c
Andrew Duong <duongandrews@gmail.com>
2019-08-20 21:14:46 +0000

    Created a message parameter in the assertEqual function

---
+++ Week_1/lotide/assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(arr1, arr2, expected) {
+  if (eqArrays(arr1, arr2) === expected) {
+    console.log(`Assertion Passed: First Array: ${arr1} Second Array: ${arr2} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: First Array: ${arr1} Second Array: ${arr2} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+assertEqual([1, 2, 3], [1, 2, 3], true);
