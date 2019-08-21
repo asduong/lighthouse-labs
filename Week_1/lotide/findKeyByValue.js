75c84de64229e981b425161379d4e8a88a12831d
Andrew Duong <duongandrews@gmail.com>
2019-08-21 19:42:26 +0000

    Created an Object  Find Key by Value function

---
+++ Week_1/lotide/findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(showObj, show) {
+  for (let key in showObj) {
+    if (showObj[key] === show) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
